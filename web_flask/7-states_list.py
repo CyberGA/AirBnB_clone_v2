e5a0491928370c5aefcbb6b90fadd620746b8d68
CyberGA <etomu.joshua@gmail.com>
2024-02-18 10:12:31 -0800

    fix: teardown context

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,29 +3,21 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
-    /hbnb: display "HBNB"
-    /c/<text>: display “C ” followed by the value
-                of the text variable (replace
-                underscore _ symbols with a space)
-    /python/<text>: display “Python ”, followed by the
-                    value of the text variable (replace
-                    underscore _ symbols with a space)
-    /number/<n>: display “n is a number” only if n is an integer
-    /number_template/<n>: display a HTML page only if n is an
-                          integer: H1 tag: “Number: n”
-                          inside the tag BODY
-    /number_odd_or_even/<n>: display a HTML page only if n is an
-                             integer: H1 tag: “Number: n is
-                             even|odd” inside the tag BODY
+    /states_list: HTML page with a list of
+                  all State objects in DBStorage.
 """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from markupsafe import escape
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(context):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
 
 
 @app.route("/states_list")
@@ -35,11 +27,6 @@ def states_list():
     return render_template("7-states_list.html", states=states)
 
 
-@app.teardown_appcontext
-def teardown(context):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
